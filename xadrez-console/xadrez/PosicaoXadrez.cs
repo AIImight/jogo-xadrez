f1cf12a0c7de04eb07e1709f552834157bcdc412
Maciel Azevedo <maciel.az.neto@gmail.com>
2018-01-07 13:41:09 -0200

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/xadrez/PosicaoXadrez.cs
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -2,12 +2,12 @@
 
 namespace xadrez
 {
-    class PosicaoXadre {
+    class PosicaoXadrez {
 
         public char coluna { get; set; }
         public int linha { get; set; }
 
-        public PosicaoXadre(char coluna, int linha)
+        public PosicaoXadrez(char coluna, int linha)
         {
             this.coluna = coluna;
             this.linha = linha;
